32a52c155dbcb51e9ec4d396b712628326db69e3
starshipgoesby <144884480+starshipgoesby@users.noreply.github.com>
2024-01-31 16:56:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/will', function (req, res) {
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
 });
-app.listen(process.env.PORT || 30000);
+app.listen(process.env.PORT || 3000);
 module.exports = app;
